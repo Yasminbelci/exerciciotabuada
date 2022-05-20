00dd64cf71f59cb927fa10627b5c410908a5a9bc
Yasmin <yasmin.belci13@gmail.com>
2022-05-20 20:42:51 -0300

    Yasmin Belci

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double n, contador, produto;
+
+Console.Write("Tabuada do numero: ");
+n = Convert.ToDouble(Console.ReadLine()!);
+
+if (n < 0 || n > 10)
+{
+    Console.WriteLine("Digite o numeros de 0 a 10");
+}
+else
+{
+    contador = 0;
+    while (contador <= 10)
+    {   
+    produto = n * contador;
+
+    Console.WriteLine($"{n} x {contador++} = {produto}");
+     }
+}
